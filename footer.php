dfc944d48da4d978aeeb865eafe09077460987c8
maulinashintade <42725367+maulinashintade@users.noreply.github.com>
2018-09-22 10:54:21 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -7,6 +7,7 @@
 	<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 	<div style="background-color: #FFC0CB;">
 			<center><font size="7" color="#1E90FF ">&copyCopyright&bull;2018&bull;6701174020</font></center><br>
+			<center><a href="https://www.instagram.com/shntdewi/">Instagram</a></center>
 	</div>
 </body>
 </html>
